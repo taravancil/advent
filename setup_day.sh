308d44017963d9edde90db61a60d8add37e1958c
Tara <tbvanc@gmail.com>
2016-05-30 18:14:50 -0500

    Add script to setup a new day module
    This creates a copy of src/day_.rs, adds the module declaration to
    src/lib.rs, and adds the modules output to src/main.rs.

---
+++ setup_day.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#
+# Sets up a new day module
+
+set -eu
+
+TEMPLATE="src/day_.rs"
+DAY="${1}"
+
+setup_day() {
+        echo "Setting up day${DAY}..."
+
+        local readonly day_file="src/day${DAY}.rs"
+        local readonly day_module="day${DAY}"
+
+        if [ -f ${day_file} ]; then
+                echo "Exiting: ${day_file} already exists." >&2
+                exit 1
+        fi
+
+        cp $TEMPLATE $day_file
+        echo "pub mod ${day_module};" >> "src/lib.rs"
+        sed -i "\$i \    advent::${day_module}::output()" src/main.rs
+}
+
+setup_day
+echo "Success!"
+
+exit 0
