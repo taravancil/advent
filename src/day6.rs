3623943bc6112f6ef96efb56da5cba707375c94b
Tara <tbvanc@gmail.com>
2016-06-01 20:57:38 -0500

    Set up structs and types

--- src/day6.rs
+++ src/day6.rs
@@ -1,22 +1,46 @@
 use std::io::prelude::*;
 use std::fs::File;
 
+enum InstructionType {
+    On,
+    Off,
+    Toggle,
+}
+
+struct Instruction {
+    start: GridPosition,
+    end: GridPosition,
+    type_: InstructionType,
+}
+
+struct Light {
+    on: bool,
+}
+
+type GridRow = [Light; 1000];
+type Grid = [GridRow; 1000];
+type GridPosition = (u32, u32);
+
 #[allow(unused_variables)]
-pub fn part1(input: &str) -> u32 {
+fn part1(instructions: &Vec<Instruction>) -> u32 {
     42
 }
 
 #[allow(unused_variables)]
-pub fn part2(input: &str) -> u32 {
+fn part2(instructions: &Vec<Instruction>) -> u32 {
     42
 }
 
 pub fn result() -> (u32, u32) {
-    let mut f = File::open("data/day_").unwrap();
+    let mut f = File::open("data/day6").unwrap();
     let mut input = String::new();
     f.read_to_string(&mut input).unwrap();
 
-    (part1(&input), part2(&input))
+    let instructions: Vec<Instruction> = input.split('\n')
+        .map(|i| parse_instruction(&i))
+        .collect();
+
+    (part1(&instructions), part2(&instructions))
 }
 
 pub fn output() {
